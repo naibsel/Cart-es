d1f54674dddae0d156c908622417314042934f16
naibsel <jatsarah11@gmail.com>
2024-10-04 16:11:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    'O CSS é uma linguagem de estilização'
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
